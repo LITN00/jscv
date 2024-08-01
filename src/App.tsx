afcf859d30d1a2948a3be49853eab61d310ff2e5
LITN00 <litn2018@kaist.ac.kr>
2024-08-01 11:16:26 +0900

    react test

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const ExampleHead = styled.div``;
+
+const App = () => {
+  return (
+    <ExampleHead>
+      react page test
+    </ExampleHead>
+  );
+}
+
+export default App;
